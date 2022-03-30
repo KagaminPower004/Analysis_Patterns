6687094a8a203fe6a0899ba18b17f10487af4189
kagami <kagamin8078@protonmail.com>
2022-03-30 17:49:22 +0900

    2022年3月30日(水) 『第一部：第二章：2.2 組織階層』に記載の『分析モデル』を参考にJavaで実装。昨日の続き(その七)。
    主な修正点は下記。
    ①ファイル分割および下記のパッケージ作成を実施。
    └─java
        └─chapter02
            └─section02
                └─domain
                    ├─model
                    │  ├─area
                    │  │  └─value_object
                    │  └─organizationhierarchy
                    │      ├─check_behavior
                    │      └─value_object
                    ├─support
                    │  └─value_object
                    └─type
    
    ■課題
    →新たな気づき
    ①コードでの設計を支援するツール『JIG』の環境構築。
    おそらく、パッケージ図、クラス図などを確認することで、
    また新たなコード改善などの気づきが得られると思われる。
    
    →既出の気づき
    ①この『組織階層ルール』モジュールを起点に、
    組織階層(コンポジット)と社員(リーフ)についての
    ビジネスルール、例えば『所属組織登録ルール』みたいな？
    モノを作成するのも
    ビジネス価値的には有益かも、
    などと夢想する。

--- src/main/java/chapter02/section02/domain/support/SysOutError_Base_Message.java
+++ src/main/java/chapter02/section02/domain/support/SysOutError_Base_Message.java
@@ -1,10 +1,17 @@
 package chapter02.section02.domain.support;
 
+import chapter02.section02.domain.support.value_object.SysOutErrorMessage;
+
 //『エラーメッセージ出力基底部』クラス
 public class SysOutError_Base_Message {
 
     //コンストラクタ
-    public SysOutError_Base_Message(String kaisouMeiMessage ) {
-        throw new RuntimeException( kaisouMeiMessage ) ;
+    public SysOutError_Base_Message(String sysOutErrorMeiMessage ) {
+        //値オブジェクトにてメッセージ内容のチェック
+        SysOutErrorMessage this_SysOutErrorMessage
+                = new SysOutErrorMessage( sysOutErrorMeiMessage );
+
+        String e_Message = this_SysOutErrorMessage.SysOutErrorMessage();
+        throw new RuntimeException( e_Message ) ;
     }
 }
